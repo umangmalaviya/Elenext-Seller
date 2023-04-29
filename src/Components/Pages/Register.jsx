70ce8d15fc6d6f6b473e7cf9e58fe3a811535836
Jenish <jenish.rutilant@gmail.com>
2023-04-29 15:38:38 +0530

    22323

--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,59 +1,63 @@
 import React, { useState } from 'react'
 import "../Styles/Register.css"
-import 'react-phone-number-input/style.css';
 import PhoneInput from 'react-phone-number-input';
-import eye from "../Images/eye.png"
-import closedEye from "../Images/closed-eyes.png"
+import 'react-phone-number-input/style.css';
 import { Link } from 'react-router-dom';
 import { useFormik } from 'formik';
 
 const Register = () => {
-
-  // const [phoneNumber, setPhoneNumber] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [showPassword, setShowPassword] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const formik = useFormik({
     initialValues: {
       email: '',
       password: '',
-      mobile: '',
+      // mobile: '',
+      name: ''
     },
+
+
     validate: (values) => {
       const errors = {};
-      // perform validation logic and add error messages to the `errors` object
+
+      if (!values.name) {
+        errors.name = 'name is required';
+      }
+
       if (!values.email) {
         errors.email = 'Email is required';
       } else if (!/\S+@\S+\.\S+/.test(values.email)) {
         errors.email = 'Invalid email address';
       }
+
       if (!values.password) {
         errors.password = 'Password is required';
       } else if (values.password.length < 8) {
         errors.password = 'Password must be at least 8 characters';
       }
+
       if (!values.mobile) {
         errors.mobile = 'Mobile is required';
       } else if (isNaN(Number(values.mobile))) {
         errors.mobile = 'Mobile number must be a number';
+      } else if (!phoneNumberRegex.test(phoneNumber)) {
+        errors.mobile = 'mobile must be at least 10 characters';
       }
       return errors;
     },
+
     onSubmit: (values) => {
-      // handle form submission logic
-      console.log(values);
-      // redirect to another page
-      window.location.href = '/login';
-    },
-  })
-
-  // const handleInputChange = (event) => {
-  //   setPassword(event.target.value);
-  // }
-
-  // const toggleShowPassword = () => {
-  //   setShowPassword(!showPassword);
-  // }
+      values.preventDefault();
+      // validate phone number input
+      const isValid = /^\+[1-9]\d{1,14}$/.test(phoneNumber);
+      if (isValid) {
+        window.location.href = "/OtpFill";
+      } else {
+        alert("Phone Please");
+      }
+    }
+  },
+  )
 
   return (
     <div className="register">
@@ -63,27 +67,31 @@ const Register = () => {
           <p className="inner-register">Register</p>
 
           <p className="name">Name :</p>
-          <input className='input-tag' type="text" />
+          <input id="name" name="name" className='input-tag' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur}
+            value={formik.values.name} />
+
+          {formik.touched.name && formik.errors.name ? (<div>{formik.errors.name}</div>) : null}
 
           <p className="name">Mobile No :</p>
-          <div id="mobile" type="tel" className="mobile">
+          <div className="mobile">
             <PhoneInput
-              international
-              defaultCountry="IN"
-              // value={phoneNumber}
-              // onChange={setPhoneNumber}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.mobile}
-              maxLength={16}
+              defaultCountry="US"
+              value={phoneNumber}
+              onChange={setPhoneNumber}
+              placeholder="Enter phone number"
             />
-
-            {formik.touched.mobile && formik.errors.mobile ? (<div>{formik.errors.mobile}</div>) : null}
-
           </div>
 
+          {/* <input id="mobile" name="mobile" className='input-tag' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur}
+            value={formik.values.mobile} /> */}
+
+
+          {/* {formik.touched.mobile && formik.errors.mobile ? (<div>{formik.errors.mobile}</div>) : null} */}
+
+
           <p className="name">E-mail :</p>
-          <input className='input-tag'
+          <input
+            className='input-tag'
             id="email"
             type="email"
             onChange={formik.handleChange}
@@ -94,25 +102,27 @@ const Register = () => {
 
           <p className="name">Password :</p>
           <div className="diplay-flex">
-            <input className='input-tag'
+            <input
+              className='input-tag'
               id="password"
               type="password"
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.password} />
-
-            {formik.touched.password && formik.errors.password ? (<div>{formik.errors.password}</div>) : null}
-            {/* <button onClick={toggleShowPassword}>
-              {showPassword ? <img src={closedEye} alt=" " className='button-img' /> : <img src={eye} alt=" " className='button-img' />}
-            </button> */}
           </div>
+
+          {formik.touched.password && formik.errors.password ? (<div>{formik.errors.password}</div>) : null}
+
           <p className='long-msg'>By enrolling your mobile phone number, you consent to receive automated security notifications via text message from Amazon. Message and data rates may apply.</p>
-          <button type='submit' className='submit-btn' disabled={!formik.isValid}>Submit</button>
+
+          <button className='submit-btn' type="submit">Submit</button>
         </form>
+
         <div className="flex-ah">
           <p className='already-have'>Already Have An Account? </p>
           <Link to='/login'>Sign In.</Link>
         </div>
+
       </div>
     </div>
   )
