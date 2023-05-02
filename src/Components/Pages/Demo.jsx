9c7d61d3877f7cc3fcece419c81451ed66a294ce
Jenish <jenish.rutilant@gmail.com>
2023-05-02 16:10:05 +0530

    fsdf

--- src/Components/Pages/Demo.jsx
+++ src/Components/Pages/Demo.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import "../Styles/Demo.css"
 
 function Demo() {
+  const data = [
+    { name: 'Jan', uv: 4000 },
+    { name: 'Feb', uv: 3000 },
+    { name: 'Mar', uv: 2000 },
+    { name: 'Apr', uv: 2780 },
+    { name: 'May', uv: 1890 },
+    { name: 'Jun', uv: 100 },
+    { name: 'Jul', uv: 3490 },
+    { name: 'Aug', uv: 4000 },
+    { name: 'Sep', uv: 3000 },
+    { name: 'Oct', uv: 2000 },
+    { name: 'Nov', uv: 2780 },
+    { name: 'Dec', uv: 100 },
+  ];
 
   return (
-    <form>
-      <input type="email" name="email" required />
-      <button type='submit'> Hey</button>
-    </form>
+    <LineChart
+      width={400}
+      height={400}
+      data={data}
+      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+      className="chart"
+    >
+      <XAxis dataKey="name" />
+      <Tooltip />
+      <CartesianGrid stroke="#f5f5f5" />
+      <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} className="line"/>
+      <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} className="line"/>
+      <YAxis dataKey="uv" />
+      <Tooltip />
+      <CartesianGrid stroke="#f5f5f5" />
+      <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+      <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+    </LineChart>
   );
 }
 
--- src/Components/Pages/ShowFillDetails.jsx
+++ src/Components/Pages/ShowFillDetails.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Styles/ShowFillDetails.css"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const ShowFillDetails = () => {
+
+    const [checkbox3Checked, setCheckbox3Checked] = useState(false);
+
+    const handleCheckbox3Change = () => {
+        setCheckbox3Checked(!checkbox3Checked);
+    };
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div className='register'>
             <div className="container-fluid">
@@ -27,8 +41,36 @@ const ShowFillDetails = () => {
 
                                     <div className="inner-shopping-details">
                                         <div>
-                                            <input type="radio" value="Male" name="gender" /> Offer free Shipping<br />
-                                            <input type="radio" value="Female" name="gender" /> Set Fee
+                                            <div className="qwe">
+                                                <input type="radio" value="Male" name="gender" />
+                                                <div className="outer-offer-free">
+                                                    <p className='offer-free'>Offer Free Shipping</p>
+                                                    <p className='free-shipping'>Free shipping will be highlighted on your offer</p>
+                                                </div>
+                                            </div>
+                                            <div className="qwe">
+                                                <input type="radio" value="Female" name="gender" />
+                                                <div className="outer-offer-free">
+                                                    <p className='offer-free'>Set Fee</p>
+                                                    <div className="local">
+                                                        <div className="per-rate">
+                                                            <p>Local</p>
+                                                            <input type="text" />
+                                                            <span>Per Order</span>
+                                                        </div>
+                                                        <div className="per-rate">
+                                                            <p>Regional</p>
+                                                            <input type="text" />
+                                                            <span>Per Order</span>
+                                                        </div>
+                                                        <div className="per-rate">
+                                                            <p>National</p>
+                                                            <input type="text" />
+                                                            <span>Per Order</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -44,7 +86,43 @@ const ShowFillDetails = () => {
                             </h2>
                             <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                 <div className="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    <p className='to-verify-your'>To verify your GSTIN details, a One Time Password (OTP) will be sent to mobile number and email address of the primary GSTIN holder. Please complete the below verification to proceed. OTP will be valid only for 30 minutes.</p>
+                                    <div className="both-side-part">
+                                        <div className="left-side-part">
+                                            <div className="account-text">
+                                                <p>Account holder Name</p>
+                                                <input type="text" />
+                                            </div>
+                                            <div className="account-text">
+                                                <p>Account Number</p>
+                                                <input type="text" />
+                                            </div>
+                                            <div className="ifsc-flex">
+                                                <p>Do You Have IFSC code ?</p>
+                                                <input type="radio" id="html" name="fav_language" value="HTML" />
+                                                <label for="html">Yes</label><br />
+                                                <input type="radio" id="css" name="fav_language" value="CSS" />
+                                                <label for="css">No</label><br></br>
+                                            </div>
+                                            <div className="ifsc-flex">
+                                                <p style={{ background: "#00BA00", padding: "0px 5px", color: "white", borderRadius: "50%" }}>&#x2713;</p> <p>verify you IFSC code success </p>
+                                            </div>
+                                        </div>
+                                        <div className="right-side-part">
+                                            <div className="account-text">
+                                                <p>Account Type</p>
+                                                <input type="text" />
+                                            </div>
+                                            <div className="account-text">
+                                                <p>Re enter account Name</p>
+                                                <input type="text" />
+                                            </div>
+                                            <div className="account-text">
+                                                <p>Upload signature</p>
+                                                <input id="file-upload" type="file" />
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -58,7 +136,36 @@ const ShowFillDetails = () => {
                             </h2>
                             <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                                 <div className="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    <p className='to-verify-your'>Update your tex details for SURAT:</p>
+
+                                    <input type="radio" id="html" name="fav_language" value="I have GSTIN number" checked={checkbox3Checked} onChange={handleCheckbox3Change} />
+                                    <label for="html" className='i-have-gst'>I have GSTIN number</label>
+
+                                    {checkbox3Checked && (
+                                        <div className="both-side-part">
+                                            <div className="left-side-part">
+                                                <div className="account-text">
+                                                    <p>Seller legal name</p>
+                                                    <input type="text" />
+                                                </div>
+                                                <div className="account-text">
+                                                    <p>Pan number</p>
+                                                    <input type="text" />
+                                                </div>
+                                            </div>
+                                            <div className="right-side-part">
+                                                <div className="account-text">
+                                                    <p>GST Number</p>
+                                                    <input type="text" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+
+                                    <div style={{ display: "flex", gap: "5px", margin: "10px 0" }}>
+                                        <input type="checkbox" />
+                                        <p className='i-want-to-only'>I want to only sell in gst exempt category like books</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -72,7 +179,22 @@ const ShowFillDetails = () => {
                             </h2>
                             <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                                 <div className="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    <p className='to-verify-your'>To verify your GSTIN details, a One Time Password (OTP) will be sent to mobile number and email address of the primary GSTIN holder. Please complete the below verification to proceed. OTP will be valid only for 30 minutes.</p>
+                                    <Button className='generate-otp' onClick={handleShow}>
+                                        Generate OTP
+                                    </Button>
+                                    <Modal show={show} onHide={handleClose}>
+                                        <Modal.Body>
+                                            <div className="main-input-otp">
+                                                <input type="text" placeholder='Please Enter Your OTP ' />
+                                                <button>Verify OTP</button>
+                                                <div className="ifsc-flex">
+                                                    <p style={{ background: "#00BA00", padding: "0px 5px", color: "white", borderRadius: "50%" }}>&#x2713;</p> <p style={{ color: "#979797" }}>Verify your Bank Account </p>
+                                                    <p style={{ color: "#1E30D9", cursor: "pointer" }}>Resend</p>
+                                                </div>
+                                            </div>
+                                        </Modal.Body>
+                                    </Modal>
                                 </div>
                             </div>
                         </div>
@@ -86,9 +208,23 @@ const ShowFillDetails = () => {
                             </h2>
                             <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                                 <div className="accordion-body">
-                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    <p className='to-verify-your'>PTC (Product Tax code) are codes which are mapped to GST rates notified by the government (i.e. the percentage of taxes which are applicable for the sale of products or services). Seller Central Account tax calculation system uses PTC to calculate the tax on the products or services a seller lists on the marketplace. This is the Tax amount which sellers will later have to remit to the GST authorities on the product or services sold/provided by them</p>
+                                    <h5 style={{ margin: "10px 0" }}>Default product Tex code</h5>
+                                    <select name="cars" id="cars">
+                                        <option disabled value="volvo">Select Product Tax Code</option>
+                                        <option value="saab">GST.0</option>
+                                        <option value="opel">GST.3</option>
+                                        <option value="audi">GST.5</option>
+                                        <option value="audi">GST.12</option>
+                                        <option value="audi">GST.18</option>
+                                        <option value="audi">GST.28</option>
+                                    </select><br />
+                                    <button className='save'>Save</button>
                                 </div>
                             </div>
+                        </div>
+                        <div className="none-div">
+                            <button className='syb'>Start Your Business</button>
                         </div>
                     </div>
                 </div>
