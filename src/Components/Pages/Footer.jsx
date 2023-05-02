83ceb182a5541e297e801bf4e9196d49455e9818
umangmalaviya <umang.rutilant@gmail.com>
2023-05-02 14:30:19 +0530

    update - 20230502

--- src/Components/Pages/Footer.jsx
+++ src/Components/Pages/Footer.jsx
@@ -1,79 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "../Styles/Footer.css"
-import logo from "../Images/f-logo.svg"
-import cards from "../Images/payment-method.svg"
+import logo from "../Images/logo.svg"
+import insta from "../Images/insta.svg"
+import facebook from "../Images/facebook.svg"
+import youtube from "../Images/youtube.svg"
 
 const Footer = () => {
   return (
     <>
-      <div className="top_footer">
-        <div className="sub_footer">
-          <div className="sub_f_header">
-            Get 20% Off Discount Coupon
-          </div>
-          <div className="sub_f_body">
-            By Subscribe Our Newslatter
-          </div>
-          <div className="buttonIn">
-            <input placeholder='Email address' className='f_textbox' type="email" id="enter" />
-            <button className='f_btn' id="clear">Subscribe</button>
-          </div>
-        </div>
-      </div>
-
       <footer>
         <div className='container-fluid'>
-          <div className='logo-part'>
-            <img src={logo} alt="logo" />
-          </div>
-          <div className='footer-links'>
+          <div className='footer-cnt'>
+            <div className='footer-info'>
+              <img src={logo} alt="logo" />
+              <p>Sell your products to crores of customers on
+                Elenext at 0% commission</p>
+              <Link className="customicon signup_btn" to="/wishlist">Start Selling</Link>
+            </div>
             <ul>
               <li>
-                <h3>ABOUT</h3>
-                <Link to="/contact">Contact Us</Link>
-                <Link to="/about">About Us</Link>
-                <Link to="#">Careers</Link>
-                <Link to="#">Elemnext Stories</Link>
-                <Link to="#">Press</Link>
-                <Link to="#">Elemnext Wholesale</Link>
-                <Link to="#">Corporate Information</Link>
+                <h3>Sell on Elenext</h3>
+                <Link to="#">Sell Online</Link>
+                <Link to="#">Pricing & Commission</Link>
+                <Link to="#">How it Process</Link>
+                <Link to="#">Shipping & Returns</Link>
+                <Link to="#">Grow Your Business</Link>
+                <Link to="#">Learning Hub</Link>
+                <Link to="#">Elenext Ads</Link>
+                <Link to="#">Shop Online on Elenext</Link>
               </li>
               <li>
-                <h3>HELP</h3>
-                <Link to="#">Payments</Link>
-                <Link to="#">Shipping</Link>
-                <Link to="#">Cancellation & Returns</Link>
-                <Link to="#">FAQ</Link>
-                <Link to="#">Report Infringement</Link>
-              </li>
-              <li>
-                <h3>CONSUMER POLICY</h3>
-                <Link to="#">Return Policy</Link>
-                <Link to="#">Terms Of Use</Link>
-                <Link to="#">Security</Link>
-                <Link to="#">Privacy</Link>
-                <Link to="#">Sitemap</Link>
-                <Link to="#">Grievance Redressal</Link>
-                <Link to="#">EPR Compliance</Link>
-              </li>
-              <li>
-                <h4>Registered Office Address:</h4>
-                <p>Elemnext Internet Private Limited,
-                  Buildings Alyssa, Begonia &
-                  Clove Embassy Tech Village,
-                  Outer Ring Road, Devarabeesanahalli Village,
-                  Bengaluru, 560103,
-                  Karnataka, India</p>
-                <p>CIN : U51109KA2012PTC066107</p>
-                <p>Telephone: 044-45614700</p>
+                <h3>Contact Us</h3>
+                <p>sell@elenext.com</p>
+                <div className='social_platform'>
+                  <img src={insta} alt="insta" />
+                  <img src={facebook} alt="facebook" />
+                  <img src={youtube} alt="youtube" />
+                </div>
               </li>
             </ul>
           </div>
-          <div className='policy-part'>
-            <p>©2022 Elenext All rights reserved</p>
-            <img src={cards} alt="payment-method" />
-          </div>
+          <h4>© 2015-22 Elenext Inc. All Rights Reserved.</h4>
         </div>
       </footer>
     </>
--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -7,17 +7,32 @@ const Navbar = () => {
   return (
     <>
       <header>
-        <div className="middle-header">
-          <div className="container-fluid">
-            <div className="middle-header-inn">
-              <Link className="navbar-brand" to="/"><img src={logo} alt="logo" /></Link>
+        <nav class="navbar navbar-expand-lg navbar-LIGHT bg-LIGHT" aria-label="Eighth navbar example">
+          <div class="container-fluid">
+            <Link class="navbar-brand" to="index.html"><img src={logo} alt="logo" /></Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample07">
+              <ul class="navbar-nav navpadle text-center">
+                <li class="nav-item navspace">
+                  <Link class="nav-link active" to="/">How it Process</Link>
+                </li>
+                <li class="nav-item navspace">
+                  <Link class="nav-link" to="#">Pricing & Commission</Link>
+                </li>
+                <li class="nav-item navspace">
+                  <Link class="nav-link" to="#">Grow Business</Link>
+                </li>
+              </ul>
               <div className="login-option">
                 <Link to="#" className="customicon login_btn">Log In</Link>
-                <Link className="customicon signup_btn" to="/wishlist">Sign Up</Link>
+                <Link className="customicon signup_btn" to="/wishlist">Start Selling</Link>
               </div>
             </div>
           </div>
-        </div>
+        </nav>
       </header>
     </>
   )
