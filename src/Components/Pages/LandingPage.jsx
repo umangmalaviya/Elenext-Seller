9813c7a09f896f6bb5e1181f2a3724acc7af5cac
umangmalaviya <umang.rutilant@gmail.com>
2023-05-02 18:00:30 +0530

    update - 20230502(3)

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "../Styles/LandingPage.css"
+import RightArrow from "../Images/right-arrow.svg"
 
 const LandingPage = () => {
     const [mobileNumber, setMobileNumber] = useState("");
@@ -48,12 +49,67 @@ const LandingPage = () => {
                 <div className='container-fluid'>
                     <div className='achievment_inn'>
                         <div className='achievment_cnt'>
-                            
+                            <h2>11 Lakh+</h2>
+                            <h4>Trust Elenext to sell online</h4>
+                        </div>
+                        <div className='achievment_cnt'>
+                            <h2>14 Crore+</h2>
+                            <h4>Customers buying across India</h4>
+                        </div>
+                        <div className='achievment_cnt'>
+                            <h2>28000+</h2>
+                            <h4>Pincode Supported for delivery</h4>
+                        </div>
+                        <div className='achievment_cnt'>
+                            <h2>700+</h2>
+                            <h4>Categories to sell online</h4>
                         </div>
                     </div>
                 </div>
             </section>
             {/* achievment_section */}
+
+            {/* process-section */}
+            <section className='process_section'>
+                <div className='container-fluid'>
+                    <h2>How it Process ?</h2>
+                    <div className='process_inn'>
+                        <div className='process_cnt'>
+                            <div className='process_info'>
+                                <img src={require('../Images/introduction-selling.png')} alt="" />
+                                <h3>Introduction to Selling on Elenext</h3>
+                                <p>Make your products available to crores of customers & businesses 24x7</p>
+                                <Link to="#">See More <img src={RightArrow} alt="right arrow" /></Link>
+                            </div>
+                        </div>
+                        <div className='process_cnt'>
+                            <div className='process_info'>
+                                <img src={require('../Images/introduction-selling.png')} alt="" />
+                                <h3>Seller fee & Pricing</h3>
+                                <p>Learn about the dashboard to create your seller account and manage your business</p>
+                                <Link to="#">See More <img src={RightArrow} alt="right arrow" /></Link>
+                            </div>
+                        </div>
+                        <div className='process_cnt'>
+                            <div className='process_info'>
+                                <img src={require('../Images/introduction-selling.png')} alt="" />
+                                <h3>Why Elenext looks at suppliers ?</h3>
+                                <p>All the benefits that come with selling on Elenext are designed to help you sell more, and make it easier to grow your business.</p>
+                                <Link to="#">See More <img src={RightArrow} alt="right arrow" /></Link>
+                            </div>
+                        </div>
+                        <div className='process_cnt'>
+                            <div className='process_info'>
+                                <img src={require('../Images/introduction-selling.png')} alt="" />
+                                <h3>Refer & earn rewards</h3>
+                                <p>Refer new sellers on Elenext.in & earn minimum rewards worth INR 2,000. Your friend earns a minimum reward worth INR 500, once they launch as a seller</p>
+                                <Link to="#">See More <img src={RightArrow} alt="right arrow" /></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* process-section */}
         </>
     )
 }
