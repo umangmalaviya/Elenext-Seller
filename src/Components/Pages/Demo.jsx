4466784215da123bae462b965190a697dfea76dd
Jenish <jenish.rutilant@gmail.com>
2023-05-05 11:28:07 +0530

    update - 0505

--- src/Components/Pages/Demo.jsx
+++ src/Components/Pages/Demo.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { NavLink, useLocation } from "react-router-dom";
-import { FaHome, FaCog, FaSignOutAlt } from "react-icons/fa";
+import React from "react";
+// import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap";
+// import { NavLink, useLocation } from "react-router-dom";
+// import { FaHome, FaCog, FaSignOutAlt } from "react-icons/fa";
 import "../Styles/Demo.css"
 
 const Demo = () => {
-  const [expanded, setExpanded] = useState(false);
-  const location = useLocation();
+  // const [expanded, setExpanded] = useState(false);
+  // const location = useLocation();
 
-  const handleToggle = () => {
-    setExpanded(!expanded);
-  };
+  // const handleToggle = () => {
+  //   setExpanded(!expanded);
+  // };
 
   return (
-    <div>
-      <Line data={data} options={options} />
-    </div>
+    <>
+      {/* <Line data={data} options={options} /> */}
+    </>
   );
 };
 
