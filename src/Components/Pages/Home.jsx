64e514289d889e58247f4a844dd8a549c4c422c6
Jenish <jenish.rutilant@gmail.com>
2023-05-06 12:24:00 +0530

    xcv

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -13,6 +13,7 @@ import s1 from "../Images/s1.svg"
 import s2 from "../Images/s2.svg"
 import s3 from "../Images/s3.svg"
 import big_sale from "../Images/big_sale.svg"
+import SellerNavabar from "./SellerNavabar";
 
 const Home = () => {
     const [expanded, setExpanded] = useState(false);
@@ -42,53 +43,9 @@ const Home = () => {
     return (
         <>
             <div className="home-display-flex">
-                <Navbar bg="light" expand="lg" expanded={expanded} className="sidenavbar">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="flex-column">
-                            <img src={logo} alt="logo" className="logo" />
-                            <LinkContainer to="/Home" exact>
-                                <Nav.Link as={NavLink} to="/Inventory" exact ativeclassname="active">
-                                    <FaHome className="mr-2" size='25px' /> Home
-                                </Nav.Link>
-                            </LinkContainer>
-
-                            <LinkContainer to="/Inventory" exact>
-                                <Nav.Link as={NavLink} to="/Inventory" exact ativeclassname="active">
-                                    <MdInventory className="mr-2" size='25px' /> Inventory
-                                </Nav.Link>
-                            </LinkContainer>
-
-                            <NavDropdown title="Inventory" id="basic-nav-dropdown">
-                                <LinkContainer to="/settings/inventoryhealth">
-                                    <NavDropdown.Item as={NavLink} to="/settings/inventoryhealth" ativeclassname="active">
-                                        Inventory Health
-                                    </NavDropdown.Item>
-                                </LinkContainer>
-                            </NavDropdown>
-
-                            <LinkContainer to="/logout">
-                                <Nav.Link as={NavLink} to="/logout" ativeclassname="active">
-                                    <FaSignOutAlt className="mr-2" /> Logout
-                                </Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-{/* <Sidebar/> */}
+                <Sidebar />
                 <div className="sidenavbar-head">
-                    <div className="sidenavbar-head-inner">
-                        <div className="left-sidenavbar-head">
-                            <p>Welcome back, FAB 2 FASHION</p>
-                            <span>Manage and grow your business with Elenext</span>
-                        </div>
-                        <div className="right-sidenavbar-head">
-                            <i className="fa-solid fa-comment" style={{ color: '#ffffff' }}></i>
-                            <i className="fa-solid fa-bell"></i>
-                            <img src={require("../Images/profile.png")} alt=" " />
-                        </div>
-                    </div>
-                    <hr />
+                    <SellerNavabar />
                     <div className="seller-container">
                         <div className="middle-sidenavbar-inner">
                             <div className="chart">
--- src/Components/Pages/Sidebar.jsx
+++ src/Components/Pages/Sidebar.jsx
@@ -5,12 +5,10 @@ import { NavLink } from "react-router-dom";
 import { MdInventory, MdSettings } from "react-icons/md";
 import "../Styles/Home.css"
 import logo from "../Images/new-seller-logo.svg"
-import { FaHome } from "react-icons/fa";
-import { RiInboxArchiveFill } from "react-icons/ri";
-import { HiCurrencyDollar } from "react-icons/hi";
-import { CiDiscount1 } from "react-icons/ci";
-import { TbSpeakerphone, TbLogout } from "react-icons/tb";
-import { TfiHeadphoneAlt } from "react-icons/tfi";
+import { FaHome, FaPercentage } from "react-icons/fa";
+import { RiCustomerServiceFill } from "react-icons/ri";
+import { HiCurrencyDollar, HiSpeakerphone } from "react-icons/hi";
+import { TbLogout } from "react-icons/tb";
 
 const Sidebar = () => {
 
@@ -27,17 +25,19 @@ const Sidebar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="flex-column">
                         <img src={logo} alt="logo" className="logo" />
-                        <LinkContainer to="/Home" exact="true">
-                            <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
-                                <FaHome className="mr-2" size='25px' /> Home
-                            </Nav.Link>
-                        </LinkContainer>
+                        <div className="upside-navbar">
+                            <LinkContainer to="/Home" exact="true">
+                                <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
+                                    <FaHome className="mr-2" size='25px' /> Home
+                                </Nav.Link>
+                            </LinkContainer>
 
-                        <LinkContainer to="/Inventory" exact="true">
-                            <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
-                                <MdInventory className="mr-2" size='25px' /> Inventory
-                            </Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/Inventory" exact="true">
+                                <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
+                                    <MdInventory className="mr-2" size='25px' /> Inventory
+                                </Nav.Link>
+                            </LinkContainer>
+                        </div>
 
                         {/* <LinkContainer to="/Demo" exact='true'>
                                 <Nav.Link as={NavLink} to="/Demo" exact="true" ativeclassname="active">
@@ -55,41 +55,43 @@ const Sidebar = () => {
                                 </Nav.Link>
                             </LinkContainer> */}
 
-                        <LinkContainer to="/Price" exact="true">
-                            <Nav.Link as={NavLink} to="/Price" exact="true" ativeclassname="active">
-                                <HiCurrencyDollar className="mr-2" size='25px' /> Price Recommendation
-                            </Nav.Link>
-                        </LinkContainer>
+                        <div className="downside-navbar">
+                            <LinkContainer to="/Price" exact="true">
+                                <Nav.Link as={NavLink} to="/Price" exact="true" ativeclassname="active">
+                                    <HiCurrencyDollar className="mr-2" size='25px' /> Price Recommendation
+                                </Nav.Link>
+                            </LinkContainer>
 
-                        <LinkContainer to="/Promotions" exact="true">
-                            <Nav.Link as={NavLink} to="/Promotions" exact="true" ativeclassname="active">
-                                <CiDiscount1 className="mr-2" size='25px' /> Promotions
-                            </Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/Promotions" exact="true">
+                                <Nav.Link as={NavLink} to="/Promotions" exact="true" ativeclassname="active">
+                                    <FaPercentage className="mr-2" size='25px' /> Promotions
+                                </Nav.Link>
+                            </LinkContainer>
 
-                        <LinkContainer to="/Advertisement" exact="true">
-                            <Nav.Link as={NavLink} to="/Advertisement" exact="true" ativeclassname="active">
-                                <TbSpeakerphone className="mr-2" size='25px' /> Advertisement
-                            </Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/Advertisement" exact="true">
+                                <Nav.Link as={NavLink} to="/Advertisement" exact="true" ativeclassname="active">
+                                    <HiSpeakerphone className="mr-2" size='25px' /> Advertisement
+                                </Nav.Link>
+                            </LinkContainer>
 
-                        <LinkContainer to="/Settings" exact="true">
-                            <Nav.Link as={NavLink} to="/Settings" exact="true" ativeclassname="active">
-                                <MdSettings className="mr-2" size='25px' /> Settings
-                            </Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/Settings" exact="true">
+                                <Nav.Link as={NavLink} to="/Settings" exact="true" ativeclassname="active">
+                                    <MdSettings className="mr-2" size='25px' /> Settings
+                                </Nav.Link>
+                            </LinkContainer>
 
-                        <LinkContainer to="/Help" exact="true">
-                            <Nav.Link as={NavLink} to="/Help" exact="true" ativeclassname="active">
-                                <TfiHeadphoneAlt className="mr-2" size='25px' /> Help & Support
-                            </Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/Help" exact="true">
+                                <Nav.Link as={NavLink} to="/Help" exact="true" ativeclassname="active">
+                                    <RiCustomerServiceFill className="mr-2" size='25px' /> Help & Support
+                                </Nav.Link>
+                            </LinkContainer>
 
-                        <LinkContainer to="/LogOut" exact="true" className="red">
-                            <Nav.Link as={NavLink} to="/LogOut" exact="true" ativeclassname="active" >
-                                <TbLogout className="mr-2" size='25px' /> Log Out
-                            </Nav.Link>
-                        </LinkContainer>
+                            <LinkContainer to="/LogOut" exact="true" className="red">
+                                <Nav.Link as={NavLink} to="/LogOut" exact="true" ativeclassname="active" >
+                                    <TbLogout className="mr-2" size='25px' /> Log Out
+                                </Nav.Link>
+                            </LinkContainer>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
