b115bcf81ccb6c545a178ec13350637131906eca
Jenish <jenish.rutilant@gmail.com>
2023-05-06 12:03:17 +0530

    fhgh

---
+++ src/Components/Pages/SellerNavabar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SellerNavabar = () => {
+    return (
+        <div className="sidenavbar-head">
+            <div className="sidenavbar-head-inner">
+                <div className="left-sidenavbar-head">
+                    <p>Welcome back, FAB 2 FASHION</p>
+                    <span>Manage and grow your business with Elenext</span>
+                </div>
+                <div className="right-sidenavbar-head">
+                    <i className="fa-solid fa-comment" style={{ color: '#ffffff' }}></i>
+                    <i className="fa-solid fa-bell"></i>
+                    <img src={require("../Images/profile.png")} alt=" " />
+                </div>
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+export default SellerNavabar
--- src/Components/Pages/Sidebar.jsx
+++ src/Components/Pages/Sidebar.jsx
@@ -22,25 +22,24 @@ const Sidebar = () => {
 
     return (
         <>
-            <div className="home-display-flex">
-                <Navbar bg="light" expand="lg" expanded={expanded} className="sidenavbar">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="flex-column">
-                            <img src={logo} alt="logo" className="logo" />
-                            <LinkContainer to="/Home" exact="true">
-                                <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
-                                    <FaHome className="mr-2" size='25px' /> Home
-                                </Nav.Link>
-                            </LinkContainer>
+            <Navbar bg="light" expand="lg" expanded={expanded} className="sidenavbar">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="flex-column">
+                        <img src={logo} alt="logo" className="logo" />
+                        <LinkContainer to="/Home" exact="true">
+                            <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
+                                <FaHome className="mr-2" size='25px' /> Home
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/Inventory" exact="true">
-                                <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
-                                    <MdInventory className="mr-2" size='25px' /> Inventory
-                                </Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/Inventory" exact="true">
+                            <Nav.Link as={NavLink} to="/Inventory" exact="true" ativeclassname="active">
+                                <MdInventory className="mr-2" size='25px' /> Inventory
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            {/* <LinkContainer to="/Demo" exact='true'>
+                        {/* <LinkContainer to="/Demo" exact='true'>
                                 <Nav.Link as={NavLink} to="/Demo" exact="true" ativeclassname="active">
                                     <RiInboxArchiveFill className="mr-2" size='25px' />
                                     <NavDropdown title="Return Product" id="basic-nav-dropdown">
@@ -56,60 +55,44 @@ const Sidebar = () => {
                                 </Nav.Link>
                             </LinkContainer> */}
 
-                            <LinkContainer to="/Price" exact="true">
-                                <Nav.Link as={NavLink} to="/Price" exact="true" ativeclassname="active">
-                                    <HiCurrencyDollar className="mr-2" size='25px' /> Price Recommendation
-                                </Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/Price" exact="true">
+                            <Nav.Link as={NavLink} to="/Price" exact="true" ativeclassname="active">
+                                <HiCurrencyDollar className="mr-2" size='25px' /> Price Recommendation
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/Promotions" exact="true">
-                                <Nav.Link as={NavLink} to="/Promotions" exact="true" ativeclassname="active">
-                                    <CiDiscount1 className="mr-2" size='25px' /> Promotions
-                                </Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/Promotions" exact="true">
+                            <Nav.Link as={NavLink} to="/Promotions" exact="true" ativeclassname="active">
+                                <CiDiscount1 className="mr-2" size='25px' /> Promotions
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/Advertisement" exact="true">
-                                <Nav.Link as={NavLink} to="/Advertisement" exact="true" ativeclassname="active">
-                                    <TbSpeakerphone className="mr-2" size='25px' /> Advertisement
-                                </Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/Advertisement" exact="true">
+                            <Nav.Link as={NavLink} to="/Advertisement" exact="true" ativeclassname="active">
+                                <TbSpeakerphone className="mr-2" size='25px' /> Advertisement
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/Settings" exact="true">
-                                <Nav.Link as={NavLink} to="/Settings" exact="true" ativeclassname="active">
-                                    <MdSettings className="mr-2" size='25px' /> Settings
-                                </Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/Settings" exact="true">
+                            <Nav.Link as={NavLink} to="/Settings" exact="true" ativeclassname="active">
+                                <MdSettings className="mr-2" size='25px' /> Settings
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/Help" exact="true">
-                                <Nav.Link as={NavLink} to="/Help" exact="true" ativeclassname="active">
-                                    <TfiHeadphoneAlt className="mr-2" size='25px' /> Help & Support
-                                </Nav.Link>
-                            </LinkContainer>
+                        <LinkContainer to="/Help" exact="true">
+                            <Nav.Link as={NavLink} to="/Help" exact="true" ativeclassname="active">
+                                <TfiHeadphoneAlt className="mr-2" size='25px' /> Help & Support
+                            </Nav.Link>
+                        </LinkContainer>
 
-                            <LinkContainer to="/LogOut" exact="true" className="red">
-                                <Nav.Link as={NavLink} to="/LogOut" exact="true" ativeclassname="active" >
-                                    <TbLogout className="mr-2" size='25px' /> Log Out
-                                </Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-
-                <div className="sidenavbar-head">
-                    <div className="sidenavbar-head-inner">
-                        <div className="left-sidenavbar-head">
-                            <p>Welcome back, FAB 2 FASHION</p>
-                            <span>Manage and grow your business with Elenext</span>
-                        </div>
-                        <div className="right-sidenavbar-head">
-                            <i className="fa-solid fa-comment" style={{ color: '#ffffff' }}></i>
-                            <i className="fa-solid fa-bell"></i>
-                            <img src={require("../Images/profile.png")} alt=" " />
-                        </div>
-                    </div>
-                    <hr />
-                </div>
-            </div>
+                        <LinkContainer to="/LogOut" exact="true" className="red">
+                            <Nav.Link as={NavLink} to="/LogOut" exact="true" ativeclassname="active" >
+                                <TbLogout className="mr-2" size='25px' /> Log Out
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     )
 }
