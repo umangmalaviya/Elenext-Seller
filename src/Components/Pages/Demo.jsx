ccbbac9a1c6cba155c754e059a8dc560c147303f
Jenish <jenish.rutilant@gmail.com>
2023-05-06 17:58:17 +0530

    gfh

--- src/Components/Pages/Demo.jsx
+++ src/Components/Pages/Demo.jsx
@@ -1,23 +1,81 @@
-import React from "react";
-// import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-// import { LinkContainer } from "react-router-bootstrap";
-// import { NavLink, useLocation } from "react-router-dom";
-// import { FaHome, FaCog, FaSignOutAlt } from "react-icons/fa";
+import React, { useState } from "react";
+import {
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList,
+} from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import "../Styles/Demo.css"
-import Sidebar from "./Sidebar";
 
-const Demo = () => {
-  // const [expanded, setExpanded] = useState(false);
-  // const location = useLocation();
-
-  // const handleToggle = () => {
-  //   setExpanded(!expanded);
-  // };
-
+const Demo = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/",
+      name: "Dashboard",
+      icon: <FaTh />,
+    },
+    {
+      path: "/about",
+      name: "About",
+      icon: <FaUserAlt />,
+    },
+    {
+      path: "/analytics",
+      name: "Analytics",
+      icon: <FaRegChartBar />,
+    },
+    {
+      path: "/comment",
+      name: "Comment",
+      icon: <FaCommentAlt />,
+    },
+    {
+      path: "/product",
+      name: "Product",
+      icon: <FaShoppingBag />,
+    },
+    {
+      path: "/productList",
+      name: "Product List",
+      icon: <FaThList />,
+    },
+  ];
   return (
-    <>
-      <Sidebar />
-    </>
+    <div className="container-fluid">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar123">
+        <div className="top_section123">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo123">
+            Logo
+          </h1>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars123">
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link123"
+            activeclassName="active"
+          >
+            <div className="icon123">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text123"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main>{children}</main>
+    </div>
   );
 };
 
