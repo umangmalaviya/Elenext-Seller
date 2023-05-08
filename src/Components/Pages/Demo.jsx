38963b225f816fcf67b22c821ed18b06269e010f
umangmalaviya <umang.rutilant@gmail.com>
2023-05-08 17:57:39 +0530

    update - 20230508

--- src/Components/Pages/Demo.jsx
+++ src/Components/Pages/Demo.jsx
@@ -1,82 +1,93 @@
-import React, { useState } from "react";
-import {
-  FaTh,
-  FaBars,
-  FaUserAlt,
-  FaRegChartBar,
-  FaCommentAlt,
-  FaShoppingBag,
-  FaThList,
-} from "react-icons/fa";
-import { NavLink } from "react-router-dom";
-import "../Styles/Demo.css"
+import "../Styles/Demo.css";
+import ReactPaginate from "react-paginate";
+import { useEffect, useState } from "react";
+// import PaginationJson from "../Styles/Data/"
 
-const Demo = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(true);
-  const toggle = () => setIsOpen(!isOpen);
-  const menuItem = [
-    {
-      path: "/",
-      name: "Dashboard",
-      icon: <FaTh />,
-    },
-    {
-      path: "/about",
-      name: "About",
-      icon: <FaUserAlt />,
-    },
-    {
-      path: "/analytics",
-      name: "Analytics",
-      icon: <FaRegChartBar />,
-    },
-    {
-      path: "/comment",
-      name: "Comment",
-      icon: <FaCommentAlt />,
-    },
-    {
-      path: "/product",
-      name: "Product",
-      icon: <FaShoppingBag />,
-    },
-    {
-      path: "/productList",
-      name: "Product List",
-      icon: <FaThList />,
-    },
-  ];
+
+function Demo() {
+  const [items, setItems] = useState([]);
+
+  const [pageCount, setpageCount] = useState(0);
+
+  let limit = 10;
+
+  useEffect(() => {
+    const getComments = async () => {
+      const res = await fetch(
+        // `http://localhost:3004/comments?_page=1&_limit=${limit}`
+        `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
+      );
+      const data = await res.json();
+      const total = res.headers.get("x-total-count");
+      setpageCount(Math.ceil(total / limit));
+      // console.log(Math.ceil(total/12));
+      setItems(data);
+    };
+
+    getComments();
+  }, [limit]);
+
+  const fetchComments = async (currentPage) => {
+    const res = await fetch(
+      // `http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
+      `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
+
+    let currentPage = data.selected + 1;
+
+    const commentsFormServer = await fetchComments(currentPage);
+
+    setItems(commentsFormServer);
+    // scroll to the top
+    //window.scrollTo(0, 0)
+  };
   return (
-    <div className="container-fluid">
-      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar123">
-        <div className="top_section123">
-          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo123">
-            Logo
-          </h1>
-          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars123">
-            <FaBars onClick={toggle} />
-          </div>
-        </div>
-        {menuItem.map((item, index) => (
-          <NavLink
-            to={item.path}
-            key={index}
-            className="link123"
-            activeclassName="active"
-          >
-            <div className="icon123">{item.icon}</div>
-            <div
-              style={{ display: isOpen ? "block" : "none" }}
-              className="link_text123"
-            >
-              {item.name}
+    <div className="container">
+      <div className="row m-2">
+        {items.map((item) => {
+          return (
+            <div key={item.id} className="col-sm-6 col-md-4 v my-2">
+              <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
+                <div className="card-body">
+                  <h5 className="card-title text-center h2">Id :{item.id} </h5>
+                  <h6 className="card-subtitle mb-2 text-muted text-center">
+                    {item.email}
+                  </h6>
+                  <p className="card-text">{item.body}</p>
+                </div>
+              </div>
             </div>
-          </NavLink>
-        ))}
+          );
+        })}
       </div>
-      <main>{children}</main>
+
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
     </div>
   );
-};
+}
 
 export default Demo;
