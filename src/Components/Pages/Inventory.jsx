2049131ab151a85a12ac38dabd9c475f21c15d93
umangmalaviya <umang.rutilant@gmail.com>
2023-05-09 16:00:35 +0530

    update - 20230509

--- src/Components/Pages/Inventory.jsx
+++ src/Components/Pages/Inventory.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { NavLink } from "react-router-dom";
-import { FaHome, FaSignOutAlt } from "react-icons/fa";
-import { MdInventory } from "react-icons/md";
+// import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap";
+// import { NavLink } from "react-router-dom";
+// import { FaHome, FaSignOutAlt } from "react-icons/fa";
+// import { MdInventory } from "react-icons/md";
 import "../Styles/Inventory.css"
-import logo from "../Images/new-seller-logo.svg"
+// import logo from "../Images/new-seller-logo.svg"
 import { Link } from 'react-router-dom'
 import FilterIcon from "../Images/filter-icon.svg"
 import FilterMenu from "../Images/filter-menu.svg"
 import Sidebar from "./Sidebar";
 import SellerNavabar from "./SellerNavabar"
+import Data from "../Styles/Data/pagination.json"
 
 const Inventory = () => {
     const [expanded, setExpanded] = useState(false);
@@ -24,6 +25,14 @@ const Inventory = () => {
     const handleTabClick = (tab) => {
         setActiveTab(tab);
     };
+
+    const [CurrentPage, SetCurrentPage] = useState(1)
+    const RecordPerPage = 5;
+    const LastIndex = CurrentPage * RecordPerPage;
+    const FirstIndex = LastIndex - RecordPerPage;
+    const Records = Data.slice(FirstIndex, LastIndex);
+    const Npage = Math.ceil(Data.length / RecordPerPage);
+    const Number = [...Array(Npage + 1).keys()].slice(1)
 
     return (
         <>
@@ -71,174 +80,61 @@ const Inventory = () => {
                                 </div>
                             </div>
                             {activeTab === 'inventory' && (
-                                <table style={{ background: "#F5F5F5" }} className="inventory_stock">
-                                    <thead>
-                                        <tr>
-                                            <th>Product Information</th>
-                                            <th>Current Stock</th>
-                                            <th>Sale Rate/Day</th>
-                                            <th>Days On Hand</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div className="inventory_item">
-                                                    <img src={require('../Images/inventory-img.png')} alt="inventory img" />
-                                                    <div className="inventory_item_info">
-                                                        <h3>FAB 2 FASHION Anarkali Gown</h3>
-                                                        <h4>SKU ID: F2F-G1002-Blue</h4>
-                                                        <h5>FSN ID: GWNFN3TFCEQ4DFHF</h5>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                            <td>5</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                <>
+                                    <table style={{ background: "#F5F5F5" }} className="inventory_stock">
+                                        <thead>
+                                            <tr>
+                                                <th>Product Information</th>
+                                                <th>Current Stock</th>
+                                                <th>Sale Rate/Day</th>
+                                                <th>Days On Hand</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {Records.map((d, i) => (
+                                                <tr key={i}>
+                                                    <td>
+                                                        <div className="inventory_item">
+                                                            {/* <img src={require('../Images/inventory-img.png')} alt="inventory img" /> */}
+                                                            <img src={d.images} alt="image" />
+                                                            <div className="inventory_item_info">
+                                                                <h3>{d.productname}</h3>
+                                                                <h4>{d.productsku}</h4>
+                                                                <h5>{d.productfsn}</h5>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>{d.currentstock}</td>
+                                                    <td>{d.salerate}</td>
+                                                    <td>{d.daysonhand}</td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                    <nav>
+                                        <ul className="pagination">
+                                            <li className="page-item">
+                                                <a to="#" className="page-link" onClick={prePage}>Prev</a>
+                                            </li>
+                                            {
+                                                Number.map((n, i) => (
+                                                    <li className={`page-item ${CurrentPage === n ? 'active' : ''}`} key={i}>
+                                                        <a to="#" className="page-link" onClick={() => changeCPage(n)}>{n}</a>
+                                                    </li>
+                                                ))
+                                            }
+                                            <li className="page-item">
+                                                <a to="#" className="page-link" onClick={nextPage}>Next</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </>
                             )}
                             {activeTab === 'lowStock' && (
-                                <div>456</div>
+                                <div>45645</div>
                             )}
                             {activeTab === 'outOfStock' && (
-                                <div>789</div>
+                                <div>78989</div>
                             )}
                             {activeTab === 'recommend' && (
                                 <div>ABCD</div>
@@ -249,6 +145,20 @@ const Inventory = () => {
             </div>
         </>
     );
+    function prePage() {
+        if (CurrentPage !== 1) {
+            SetCurrentPage(CurrentPage - 1)
+        }
+    }
+    function changeCPage(id) {
+        SetCurrentPage(id)
+    }
+    function nextPage() {
+        if (CurrentPage !== Npage) {
+            SetCurrentPage(CurrentPage + 1)
+        }
+    }
+
 };
 
 export default Inventory;
